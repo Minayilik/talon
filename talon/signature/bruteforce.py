09a0c8db78f62762ed588a61eefb24eb54ff8d37
Minayilik <43521667+Minayilik@users.noreply.github.com>
2021-08-11 14:37:27 +0300

    Update bruteforce.py
    I don't know if we will use brute force or not but I added "Get Outlook for .." to regex ( RE_PHONE_SIGNATURE)

--- talon/signature/bruteforce.py
+++ talon/signature/bruteforce.py
@@ -39,6 +39,8 @@ RE_PHONE_SIGNATURE = re.compile(r'''
                        ^sent[ ]([\S]*[ ])?from[ ]my[ ]BlackBerry.*$
                        |
                        ^Enviado[ ]desde[ ]mi[ ]([\S]+[ ]){0,2}BlackBerry.*$
+                       |
+                       ^get[ ]Outlook[ ]{1}for[\s,!\w].*$ 
                    )
                    .*
                )
